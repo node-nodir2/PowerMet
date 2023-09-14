ca9b6ea03c89837d5ec65fd59f2f021cfeeb623a
node-nozima <xoliqulovanozima04@gmail.com>
2023-09-14 23:16:01 +0300

    Added

--- app/[lang]/head.jsx
+++ app/[lang]/head.jsx
@@ -7,7 +7,7 @@ export default function Head({ language }) {
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
             />
-            <title>{language?.title}</title>
+            <title>POWERMET</title>
             <link rel="icon" href="/favicon.ico" sizes="any" />
             <link rel="icon" href="/apple-icon.png" sizes="any" />
             <meta
--- components/Services/Services.jsx
+++ components/Services/Services.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+import Loader from "@/public/Images/spinner.svg";
+
 import { baseUrl } from "@/data";
 
 const Services = ({ lang }) => {
@@ -21,26 +23,40 @@ const Services = ({ lang }) => {
 
     return (
         <section className="container !py-[30px] lg:!py-[42px] space-y-5 lg:space-y-[30px]">
-            {datas?.map((data) => (
-                <div
-                    key={data?._id}
-                    className={`lg:flex lg:items-srart lg:justify-between border-b-[1px] border-[#333] pb-5 lg:pb-10`}
-                >
+            {datas.length > 0 ? (
+                datas?.map((data) => (
                     <div
-                        data-aos={`${
-                            data?.row ? "fade-up-left" : "fade-up-right"
-                        }`}
-                        data-aos-duration="500"
-                        className="lg:max-w-[430px] w-full"
+                        key={data?._id}
+                        className={`lg:flex lg:items-srart lg:justify-between border-b-[1px] border-[#333] pb-5 lg:pb-10`}
                     >
-                        <h2 className="font-bold text-[24px] sm:text-[30px] text-[#333] leading-8">
-                            {data?.name}
-                        </h2>
-                        <p className="font-medium text-[18px] text-text-color leading-normal mt-2">
-                            {data?.description}
-                        </p>
+                        <div
+                            data-aos={`${
+                                data?.row ? "fade-up-left" : "fade-up-right"
+                            }`}
+                            data-aos-duration="500"
+                            className="lg:max-w-[430px] w-full"
+                        >
+                            <h2 className="font-bold text-[24px] sm:text-[30px] text-[#333] leading-8">
+                                {data?.name}
+                            </h2>
+                            <p className="font-medium text-[18px] text-text-color leading-normal mt-2">
+                                {data?.description}
+                            </p>
+                            <Image
+                                className="block sm:hidden max-w-[650px] w-full h-[225px] rounded-b-[26px] mt-5 object-cover"
+                                src={`${baseUrl}/public/uploads/${data?.image}`}
+                                alt="service-image"
+                                priority={true}
+                                height={1000}
+                                width={1000}
+                            />
+                        </div>
                         <Image
-                            className="block sm:hidden w-full h-[225px] rounded-b-[26px] mt-5 object-cover"
+                            data-aos={`${
+                                data?.row ? "fade-up-right" : "fade-up-left"
+                            }`}
+                            data-aos-duration="500"
+                            className={`hidden sm:block max-w-[650px] w-full h-[426px] rounded-b-[50px] object-cover lg:ml-5`}
                             src={`${baseUrl}/public/uploads/${data?.image}`}
                             alt="service-image"
                             priority={true}
@@ -48,20 +64,14 @@ const Services = ({ lang }) => {
                             width={1000}
                         />
                     </div>
-                    <Image
-                        data-aos={`${
-                            data?.row ? "fade-up-right" : "fade-up-left"
-                        }`}
-                        data-aos-duration="500"
-                        className={`hidden sm:block w-full h-[426px] rounded-b-[50px] object-cover lg:ml-5`}
-                        src={`${baseUrl}/public/uploads/${data?.image}`}
-                        alt="service-image"
-                        priority={true}
-                        height={1000}
-                        width={1000}
-                    />
-                </div>
-            ))}
+                ))
+            ) : (
+                <Image
+                    className="block mx-auto w-[60px] sm:w-[100px] h-[60px] sm:h-[100px]"
+                    src={Loader}
+                    alt="loader"
+                />
+            )}
         </section>
     );
 };
